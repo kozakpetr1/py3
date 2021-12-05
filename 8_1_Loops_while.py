c0c3fe9786418636c967139fc51ef54319bd871d
kozakpetr1 <kozak.petr@gmail.com>
2021-12-05 21:30:23 +0100

    Loops

---
+++ 8_1_Loops_while.py
@@ -0,0 +1,26 @@
+def arithmSeq(a1, d, n):
+    return a1 + d * (n - 1)
+
+def isEven(num):
+    return num % 2 == 0
+
+print("Prvních n-členů aritmetické posloupnosti")
+firstMember = float(input("První člen posloupnosti: "))
+difference = float(input("Přírůstek: "))
+numOfMembers = int(input("Počet členů: "))
+
+i = 1
+while i <= numOfMembers:
+    print(f"a[{i}]=", arithmSeq(firstMember, difference, i), end=", ")
+    i += 1
+else:
+    print("END")
+
+print("Sudá čísla od 1 do n")
+n = int(input("Zadej n: "))
+i = 1
+while i <= n:
+    if isEven(i):
+        print(i, end=" ")
+    i += 1
+    
