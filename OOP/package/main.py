ecfbae584747172bf40626c68c255981f21f6d2a
petr@vachasoftware.com <petr@vachasoftware.com>
2022-04-11 09:37:06 +0200

    Creating Python Library Essentials

--- OOP/package/main.py
+++ OOP/package/main.py
@@ -1,7 +1,10 @@
 from textooo import Texto as T
 
-
 myText = T("What about you?")
 print(myText.getS())
+
 myText.replaS("What", "How")
+print(myText.getS())
+
+myText.replaS("Who", "How", "Who are you?")
 print(myText.getS())
