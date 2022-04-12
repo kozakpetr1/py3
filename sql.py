a1e4de6505d46d51fba5bc1505d8b2425dd29f65
petr@vachasoftware.com <petr@vachasoftware.com>
2022-04-12 08:15:28 +0200

    MySQL connection

---
+++ sql.py
@@ -0,0 +1,20 @@
+import mysql.connector as db
+from mysql.connector import Error
+
+try:
+    connection = db.connect(host='localhost', database='plang', user='root', password='')
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
