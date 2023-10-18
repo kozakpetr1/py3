04eaca6164051e5f9d4b23c6ad98ba92302cefab
petr@vachasoftware.com <petr@vachasoftware.com>
2023-10-18 12:04:47 +0200

    OOP principals

--- examples/OOP/dedicnost2a.py
+++ examples/OOP/dedicnost2a.py
@@ -1,6 +1,7 @@
 """ Dědičnost
     Usecka vytváří objekty třídy Bod v konstruktoru __init__.
     Dvě instance třídy Bod jsou pak součástí instance třídy Usecka jako atributy.
+    Jedná se o tzv. skládání.
     
 """
 
@@ -9,41 +10,51 @@ import math
 
 clear = lambda: os.system('cls')
 
-class Bod:
+class Point:
     
     def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
+        self.__x = x
+        self.__y = y
 
-    def nastavSouradnice(self, x, y):
-        self.x = x
-        self.y = y
+    def setCoords(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def getX(self):
+        return self.__x
+
+    def getY(self):
+        return self.__y
+
+    def getCoords(self):
+        return (self.getX(), self.getY())
 
     def __str__(self):
-        return f"[{self.x},{self.y}]"
+        return f"[{self.getX()},{self.getY()}]"
     
-class Usecka:
+class LineSegment:
 
     def __init__(self, u=0, v=0, x=0, y=0):
-        self.__a = Bod(u, v)
-        self.__b = Bod(x, y)
+        self.__a = Point(u, v)
+        self.__b = Point(x, y)
     
-    def nastavSouradnice(self, u, v, x, y):
-        self.__a.nastavSouradnice(u, v)
-        self.__b.nastavSouradnice(x, y)
+    def setLineSegmentCoords(self, u, v, x, y):
+        self.__a.setCoords(u, v)
+        self.__b.setCoords(x, y)
 
     def __str__(self):
-        return  f"[{self.__a.x},{self.__a.y}][{self.__b.x},{self.__b.y}]"
+        return  f"[{self.__a.getX()},{self.__a.getY()}][{self.__b.getX()},{self.__b.getY()}]"
 
-    def delkaUsecky(self):
-        return math.sqrt(math.pow(self.__a.x - self.__b.x, 2) + math.pow(self.__a.y - self.__b.y, 2))
+    def getLineSegmentLength(self):
+        return math.sqrt(math.pow(self.__a.getX() - self.__b.getX(), 2) + math.pow(self.__a.getY() - self.__b.getY(), 2))
     
 clear()
 
-B = Bod()
-B.nastavSouradnice(1, -2)
+B = Point()
+B.setCoords(1, -2)
 print(B)
 
-U = Usecka(1, 3, 4, -1)
+U = LineSegment(1, 3, 4, -1)
+U.setLineSegmentCoords(1, 3, 4, 0)
 print(U)
-print(f"{U.delkaUsecky():.2f}")
+print(f"{U.getLineSegmentLength():.2f}")
