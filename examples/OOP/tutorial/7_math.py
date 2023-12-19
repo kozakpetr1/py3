5f55b746d8eacedfc4a02cb7ccac2e9b1790e272
Petr Kozák <kozak.petr@gmail.com>
2023-12-19 08:21:16 +0100

    OOP essentials

---
+++ examples/OOP/tutorial/7_math.py
@@ -0,0 +1,40 @@
+class M:
+    
+    pi = 3.14159265358979
+    e  = 2.71828182882818
+    
+    @staticmethod
+    def abs(num) -> bool:
+        pass
+    
+    @staticmethod
+    def isEven(num) -> bool:
+        pass
+    
+    @staticmethod
+    def isOdd(num) -> bool:
+        pass
+
+    @staticmethod
+    def isPositive(num) -> bool:
+        pass
+
+    @staticmethod
+    def isNegative(num) -> bool:
+        pass
+    
+    @staticmethod
+    def isPrimeNumber(num) -> bool:
+        pass
+        
+    @staticmethod
+    def isLeapYear(num) -> bool:
+        pass
+    
+    @staticmethod
+    def isPerfectNumber(num) -> bool:
+        pass
+    
+    @staticmethod
+    def getSumOfDigits(num) -> bool:
+        pass
--- examples/OOP/tutorial/client-server/client.py
+++ examples/OOP/tutorial/client-server/client.py
@@ -5,6 +5,17 @@ from time import sleep
 import random as r
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 class Client:
     
     def __init__(self, **kwargs):
@@ -15,6 +26,7 @@ class Client:
         self.__client_socket = socket.socket()
         self.__client_socket.connect((self.__host, self.__port))  
 
+
     def go(self):
         
         os.system('cls')
@@ -24,12 +36,12 @@ class Client:
         while self.__message.lower().strip() != 'bye':
             self.__client_socket.send(self.__message.encode())
             self.__data = self.__client_socket.recv(1024).decode()
-            print('-> ', end ="")
+            print(f"-> {bcolors.OKBLUE}", end ="")
             for char in self.__data:
                 sleep(r.random() * 0.05)
                 sys.stdout.write(char)
                 sys.stdout.flush()
-            print("")
+            print(f"{bcolors.ENDC}")
             self.__message = input(" $ ")
 
         self.__client_socket.close()
