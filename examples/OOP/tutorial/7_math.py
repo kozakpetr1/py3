ec8d3d6db772811ea2395be3f7773717cafe1edf
Petr Kozák <kozak.petr@gmail.com>
2023-12-19 09:03:47 +0100

    OOP essentials

--- examples/OOP/tutorial/7_math.py
+++ examples/OOP/tutorial/7_math.py
@@ -2,39 +2,59 @@ class M:
     
     pi = 3.14159265358979
     e  = 2.71828182882818
-    
+
     @staticmethod
+    def check(func):
+        def wrap(args):
+            if isinstance(args, int):
+                return func(args)
+            else:
+                raise Exception("This number is not an integer.") 
+        return wrap
+        
+    @staticmethod
+    @check
     def abs(num) -> bool:
-        pass
+        return num if num > 0 else -num
     
     @staticmethod
+    @check
     def isEven(num) -> bool:
-        pass
+        return True if (num % 2) == 0 else False
     
     @staticmethod
+    @check
     def isOdd(num) -> bool:
-        pass
+        return not M.isEven(num)
 
     @staticmethod
+    @check
     def isPositive(num) -> bool:
-        pass
+        return True if num == M.abs(num) else False
 
     @staticmethod
+    @check
     def isNegative(num) -> bool:
-        pass
+        return not M.isPositive(num)
     
     @staticmethod
+    @check
     def isPrimeNumber(num) -> bool:
         pass
         
     @staticmethod
+    @check
     def isLeapYear(num) -> bool:
         pass
     
     @staticmethod
+    @check
     def isPerfectNumber(num) -> bool:
         pass
     
     @staticmethod
-    def getSumOfDigits(num) -> bool:
+    @check
+    def getSumOfDigits(num) -> int:
         pass
+    
+print(M.abs(-6))
